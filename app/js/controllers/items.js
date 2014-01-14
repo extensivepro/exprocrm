177cb9851bcde5901428316d2c08c003d3ed1480
Zhanwen_Xu <leonardoipod@gmail.com>
2014-01-14 17:39:54 +0800

    Add: 商品管理，close #5

--- app/js/controllers/items.js
+++ app/js/controllers/items.js
@@ -2,7 +2,7 @@ function ItemsController($scope, Items, Pagination, $timeout, $injector){
   $injector.invoke(BasicController, this, {$scope: $scope});
   $scope.resource = Items
   $scope.searchOptions.fields = ['name']
-  $scope.editView = "views/merchant/edit.html"
+  $scope.editView = "views/item/edit.html"
 
   // profile
   $scope.profileShortcuts = $scope.profileShortcuts.concat([
@@ -11,31 +11,33 @@ function ItemsController($scope, Items, Pagination, $timeout, $injector){
   ])
   $scope.profileAvatar = "img/avatar.jpg"
   $scope.profileFields = [
-    {name: "merchantID", title: "业主ID", unlist: true, hide: true},
-    {name: "code", title: "商品编码"},
+    {name: "merchantID", title: "业主ID", required: true, unlist: true, hide: true},
+    {name: "code", title: "商品编码", required: true},
     {name: "mnemonicCode", title: "商品助记码"},
-    {name: "name", title: "商品名称"},
-    {name: "price", title: "单价"},
+    {name: "name", title: "商品名称", required: true},
+    {name: "price", title: "单价", required: true},
     {name: "desc", title: "商品描述"},
     {name: "status", title: "状态", value:function(entity){
+      entity.fieldClass = entity.fieldClass || {}
       if(entity.status === 'sale') {
-        this.class = "label label-success"
+        entity.fieldClass.status = "label label-success"
         return "正常"
       } else if(entity.status === 'removed') {
-        this.class = "label label-warning"
+        entity.fieldClass.status = "label label-warning"
         return "下架"
       } else {
         return entity.status
       }
-    }, hide:true},
-    {name: "createdAt", title: "创建日期", hide: true},
+    }, hide:true, createHide: true},
+    {name: "createdAt", title: "创建日期", createHide: true, hide: true},
     {name: "model", title: "型号"},
-    {name: "updatedAt", title: "更新日期", unlist: true, hide: true}
+    {name: "updatedAt", title: "更新日期", createHide: true, unlist: true, hide: true}
   ]
 
   // route
   $scope.showCreate = function() {
-    $scope.showEdit({password: '123456'})
+    var d = new Date();
+    $scope.showEdit({createdAt: Math.round(d.getTime()/1000)})
   }
 
   // bussiness
