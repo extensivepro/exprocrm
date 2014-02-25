46eea48db32de7fee2fb2a42e6e96a971ae1dfa6
tsq <1530234656@qq.com>
2014-02-25 14:56:45 +0800

    add:增加员工所属商店 close: #94

--- app/js/services/merchants.js
+++ app/js/services/merchants.js
@@ -4,8 +4,8 @@
 
 angular.module('exproCRM.services').factory("Merchants", function($resource){
   return $resource(window.restful.baseURL+'/merchants/:merchantID', {merchantID:'@id'}, {
-    me: {method: 'GET', params:{merchantID:'me'}},
     update: { method: 'PUT' },
-    count: { method: 'GET' , params: {merchantID: 'count'}}
+    count: { method: 'GET' , params: {merchantID: 'count'}},
+    query: {method: 'GET', isArray: false}
   });
 });
