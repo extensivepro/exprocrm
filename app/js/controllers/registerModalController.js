11743bfd4a1782859edb6c643bd7042c5473c4e5
leonardean <leonardoipod@gmail.com>
2014-03-10 15:49:39 +0800

    添加注册信息验证

--- app/js/controllers/registerModalController.js
+++ app/js/controllers/registerModalController.js
@@ -7,12 +7,43 @@ function RegisterModalController($scope, $modalInstance, registerUser) {
   $scope.ok = function () {
     if ($scope.registerUser.username != "" &&
       $scope.registerUser.password != "" &&
-      $scope.registerUser.displayName != "" ){
+      $scope.registerUser.displayName != "") {
       $scope.registerUser.phone = $scope.registerUser.username;
-      $modalInstance.close($scope.registerUser);
-      console.log($scope.registerUser);
+      if (validate($scope.registerUser) == true) {
+        $modalInstance.close($scope.registerUser);
+        console.log($scope.registerUser);
+      }
     }
   };
+
+  function validate (registerUser) {
+    var usernameReg = RegExp("[a-zA-Z]");
+    if (usernameReg.test(registerUser.username) == true ||
+      $scope.registerUser.username.toString().length != 11){
+      $scope.userNameErrMess = "请输入11位手机号"
+      return false;
+    }else
+      $scope.userNameErrMess = ""
+    if ($scope.registerUser.password.toString().length < 6){
+      $scope.passwordErrMess = "密码不能少于6位"
+      return false;
+    }else
+      $scope.passwordErrMess = ""
+
+    if (registerUser.idcard.toString().length != 0)
+      if (usernameReg.test(registerUser.idcard) == true ||
+        $scope.registerUser.idcard.toString().length != 18) {
+        $scope.idcardErrMess = "请输入18位身份证号"
+        return false;
+      } else
+        $scope.idcardErrMess = ""
+    if ($scope.registerUser.password != $scope.registerUser.repassword){
+      $scope.repasswordErrMess = "两次密码输入不一致";
+      return false;
+    }else
+      $scope.repasswordErrMess = "";
+    return true;
+  }
 
   $scope.cancel = function () {
     $modalInstance.dismiss('cancel');
