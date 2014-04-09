799865466ee8d6df92e955def4de9b9bc9d8d305
guanbo <guanbo2002@gmail.com>
2014-04-09 14:09:59 +0800

    Update:改善登录需要点击2次的问题

--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,17 @@ angular.module('exproRMS.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }])
+  }]).
+  directive('formAutofillFix', function($timeout) {
+    return function(scope, elem, attrs) {
+      if(attrs.ngSubmit) {
+        $timeout(function() {
+          elem.unbind('submit').submit(function(e) {
+            e.preventDefault();
+            elem.find('input, textarea, select').trigger('input').trigger('change').trigger('keydown');
+            scope.$apply(attrs.ngSubmit);
+          });
+        });
+      }
+    };
+  });
