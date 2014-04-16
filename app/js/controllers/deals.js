ce00fe97ffc57460298edc6cbc08eb43023cfc34
wangjing199408 <1398249477@qq.com>
2014-04-16 12:39:27 +0800

    fixed 258
    新注册的用户，销售流水列表虽然为空，但是页脚分页处显示错误

--- app/js/controllers/deals.js
+++ app/js/controllers/deals.js
@@ -81,8 +81,8 @@ function DealsController($scope, Deals, Pagination, $timeout, $injector){
     ];
     $scope.isHide = true; //隐藏新增按钮
     $scope.$watch('currentShowShop.shop', function () {
-      $scope.params['shopID'] = $scope.currentShowShop.shop.id; // default use the first shop of the currentMerchant
-      $scope.countQs['shopID'] = $scope.currentShowShop.shop.id;
+       $scope.params['shopID'] = JSON.stringify({$in:$scope.currentMerchant.shopIDs}); // default use the first shop of the currentMerchant
+      $scope.countQs['shopID'] = JSON.stringify({$in:$scope.currentMerchant.shopIDs});
       $scope.refreshList();
     });
     $scope.defaultString = "buyer.name";
