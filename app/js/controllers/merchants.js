8bc3c43f64de82b1ebf18b7ce89f6b808321844d
tsq <1530234656@qq.com>
2014-04-29 18:07:35 +0800

    Add 增加微信公众号 Close #311

--- app/js/controllers/merchants.js
+++ app/js/controllers/merchants.js
@@ -1,9 +1,6 @@
 function MerchantsController($scope, Merchants, Pagination, $timeout, $injector,localStorageService, $fileUploader,$resource){
-  $injector.invoke(BasicController, this, {$scope: $scope});
+  //start: basic
   $scope.activeView = "views/merchant/profile.html"
-  $scope.resource = Merchants
-  $scope.searchOptions.fields = ['name', 'telephone']
-  $scope.searchOptions.tooltip = "请输入商户名称"
   $scope.profileAvatar = "img/avatar.jpg"
   $scope.profileFields = [
     {name: "id", title: "商户ID", unlist: true, hide:true, createHide: true, isProfileHide:true}
@@ -17,6 +14,13 @@ function MerchantsController($scope, Merchants, Pagination, $timeout, $injector,
     , {name: "address", title: "地址", required: true}
     , {name: "zip", title: "邮编", unlist: true}
     , {name: "url", title: "商户网站", unlist: true}
+    , {name: "weixin", title: "微信公众号", unlist: true, value: function (entity) {
+      if (entity.hasOwnProperty('weixin')) {
+        return entity.weixin.originID;
+      } else {
+        return '';
+      }
+    }}
     , {name: "createdAt", title: "创建日期", hide: true, createHide: true}
     , {name: "updateAt", title: "更新日期", unlist: true, hide: true, createHide: true, isProfileHide:true}
     , {name: "newestDeviceCode", title: "设备编码", hide: true, unlist: true, isProfileHide:true, createHide: true}
@@ -32,43 +36,76 @@ function MerchantsController($scope, Merchants, Pagination, $timeout, $injector,
         return "状态错误"
       }
     }, hide:true, createHide: true}
-  ]
-
-  $scope.profileShortcuts = [
-      {class: "box quick-button-small col-lg-1 col-md-2 col-xs-6", icon: "fa fa-cog", text: "设置", op:"edit"}
   ];
+  $scope.profileShortcuts = [
+      {class: "box quick-button-small col-lg-1 col-md-2 col-xs-6", icon: "fa fa-edit fa-5x", text: "编辑", op:"edit"}
+  ];
+  $scope.valueOfKeyString = function (entity, keyString) {
+    var v = entity
+    var keys = keyString.split('.')
+    var theKey = keys[0]
+    keys.forEach(function (key) {
+      theKey = key
+      v = v[key]
+    })
+    if (theKey === 'createdAt' || theKey === 'updateAt') {
+      v = new Date(v * 1000).toLocaleString()
+    }
+    return v
+  };
+  $scope.cancel = function () {
+    $scope.activeView = "views/merchant/profile.html"
+  };
+  //end: basic
 
-  // route
+
+  //start: profile
+  $scope.showProfile = function () {
+    $scope.activeView = "views/merchant/profile.html";
+  };
+  $scope.$watch('currentMerchant.merchant.logo', function () {
+    var logo = $scope.currentMerchant.merchant.logo;
+    if (logo) {
+      $scope.logoImg = window.restful.baseLogoSrcURL + logo.name;
+    }
+  });
+  //end: profile
+
+
+  //start: create
   $scope.showCreate = function() {
-    var d = new Date();
-    $scope.showEdit({createdAt: Math.round(d.getTime()/1000)})
+    $scope.entity = {};
     $scope.activeView = "views/merchant/create.html"
   }
-
   $scope.create = function(entity) {
+    if (!$scope.check('create')) {
+      return;
+    }
     entity["owner"] = $scope.me;
-    var newOne = new $scope.resource(entity);
+    entity['createdAt'] = Math.round(new Date().getTime() / 1000);
+    var newOne = new Merchants(entity);
     newOne.$save(function(user) {
-      console.log("success",user)
       Merchants.get({id:newOne.id}, function(merchant) {
         $scope.currentMerchant.merchant = merchant;
         $scope.allMerchant.push(merchant);
         $scope.activeView = "views/merchant/profile.html"
       })
     },function(err){
-      console.log('error:', err)
-    })
+      showDpdAlert(err);
+    });
   }
+  //end: create
 
-  $scope.editMerchant = function (entity) {
+
+  //start: edit
+  $scope.showEdit = function (entity) {
     $scope.entity = entity
     $scope.activeView = "views/merchant/edit.html"
   }
-
-  $scope.cancelEdit = function () {
-    $scope.activeView = "views/merchant/profile.html"
-  }
-  $scope.update = function (entity) {
+  $scope.edit = function (entity) {
+    if (!$scope.check('edit')) {
+      return;
+    }
     Merchants.update(entity, function (result) {
       Merchants.get({id:result.id}, function (merchant) {
         var allMerchants = $scope.allMerchant;
@@ -84,26 +121,9 @@ function MerchantsController($scope, Merchants, Pagination, $timeout, $injector,
         console.log('err:\n', err);
       })
     }, function (err) {
-      console.log('err:\n', err);
+      showDpdAlert(err);
     });
   }
-
-  $scope.storageType = 'Local storage';
-  if (localStorageService.getStorageType().indexOf('session') >= 0) {
-    $scope.storageType = 'Session storage'
-  }
-  if (!localStorageService.isSupported) {
-    $scope.storageType = 'Cookie';
-  }
-  $scope.local = {};
-  $scope.$watch('local.cbx', function () {
-    if ($scope.local.cbx===true) {
-      localStorageService.set('localStorageCurrentMerchant',$scope.currentMerchant.merchant);
-    }
-  })
-  $scope.defaultString = "name";
-  $scope.trackListPage.activeView = 'views/merchant/profile.html';
-
   $scope.initUpload = function () {
     var subdir = 'logos';
     var comments = '';
@@ -117,11 +137,12 @@ function MerchantsController($scope, Merchants, Pagination, $timeout, $injector,
       var obj = {
         id: $scope.currentMerchant.merchant.id,
         logo: {
-          image: res[0].filename,
-          imageID:res[0].id
+          name: res[0].filename,
+          id:res[0].id
         }
       };
       $scope.currentMerchant.merchant.logo = obj.logo;
+      console.log('obj***:\n', obj);
       Merchants.update(obj, function (result) {
       }, function (err) {
         console.log('err:\n', err);
@@ -131,7 +152,6 @@ function MerchantsController($scope, Merchants, Pagination, $timeout, $injector,
       $scope.showProfile();
     });
   }
-
   $scope.uploadInEdit = function () {
     if ($scope.uploader.queue.length > 1) {
       alert('上传图片不能超过1张！');
@@ -140,12 +160,17 @@ function MerchantsController($scope, Merchants, Pagination, $timeout, $injector,
       alert('照片大小不能大于2M');
       $scope.uploader.clearQueue();
     }else{
-      if ($scope.currentMerchant.merchant.logo) {
+      var currMerchant = $scope.currentMerchant.merchant;
+      if (!currMerchant.hasOwnProperty('logo')) {  // 从未上传过logo
+        $scope.uploader.uploadAll();
+      } else if (!currMerchant.logo.hasOwnProperty('name')){ // 上传过，但是因为一些原因图片丢失了
+        $scope.uploader.uploadAll();
+      } else { //正常情况下修改上传图片
         var Upload = $resource(window.restful.baseURL+'/upload/:id', {id:'@id'});
-        console.log('id:\n', $scope.currentMerchant.merchant.logo.imageID);
-        Upload.delete({id: $scope.currentMerchant.merchant.logo.imageID}, function () {
+        Upload.delete({id: $scope.currentMerchant.merchant.logo.id}, function () {
           $scope.uploader.uploadAll();
-        }, function (err) {
+        }, function (err) { // 删除原有图片报错，可能是图片在服务端被人手动删除了
+          alert('您的logo图片因为一些意外而丢失，请刷新页面后重新上传');
           var obj = {
             id: $scope.currentMerchant.merchant.id,
             logo: {}
@@ -156,19 +181,83 @@ function MerchantsController($scope, Merchants, Pagination, $timeout, $injector,
             console.log('err:\n', err);
           });
         });
-      } else {
-        $scope.uploader.uploadAll();
       }
     }
   }
-  $scope.showProfile = function () {
-    $scope.activeView = "views/merchant/profile.html";
-  };
-  $scope.$watch('currentMerchant.merchant.logo', function () {
-    var logo = $scope.currentMerchant.merchant.logo;
-    if (logo) {
-      $scope.logoImg = window.restful.baseLogoSrcURL + logo.image;
+  //end: edit
+
+
+  //start: localstorage
+  $scope.storageType = 'Local storage';
+  if (localStorageService.getStorageType().indexOf('session') >= 0) {
+    $scope.storageType = 'Session storage'
+  }
+  if (!localStorageService.isSupported) {
+    $scope.storageType = 'Cookie';
+  }
+  $scope.local = {};
+  $scope.$watch('local.cbx', function () {
+    if ($scope.local.cbx===true) {
+      localStorageService.set('localStorageCurrentMerchant',$scope.currentMerchant.merchant);
     }
-  })
+  });
+  //end: localstorage
+
+
+  //start: msg
+  $scope.initErrContent = function () { // 进入新增和编辑界面首先要将errContent置空
+    $scope.errContent = { // errContent：用于展示错误消息的对象
+      msg:''
+    };
+  }
+  $scope.check = function (flag) { // 校验新增和编辑时的属性值
+    var v = $scope.entity;
+    var m = $scope.errContent;
+    if (!v.name) {
+      m.msg = '商户简称不能为空';
+      return showAlert();
+    } else if (!v.fullName) {
+      m.msg = '商户全称不能为空';
+      return showAlert();
+    } else if (!(/^\d{7,11}$/.test(v.telephone))) {
+      m.msg = '号码格式不对，需为7~11数字';
+      return showAlert();
+    } else if (flag == 'create'){
+      return true;
+    } else if (v.hasOwnProperty('email') && v.email && !($scope.patternForEmail.test(v.email))) {
+      m.msg = '邮箱格式不正确';
+      return showAlert();
+    } else if (v.hasOwnProperty('zip') && v.zip && !(/^\d{6}/.test(v.zip))) {
+      m.msg = '邮编不对，需为6位数字';
+      return showAlert();
+    } else {
+      return true;
+    }
+  };
+  function showDpdAlert(err) { // 来自服务端的错误信息
+    if (err.data) {
+      if (err.data.message == 'merchant name exist') {
+        $scope.errContent.msg = '此商户简称已被占用！';
+        showAlert();
+      } else if (err.data.message == 'merchant fullName exist') {
+        $scope.errContent.msg = '此商户全名已被占用！';
+        showAlert();
+      } else if (err.data.message == 'exist originID of weixin') {
+        $scope.errContent.msg = '此微信公众号已被占用！';
+        showAlert();
+      }
+    } else {
+      console.log('err:\n', err);
+    }
+  }
+  function showAlert() { // 2秒后错误消息消失，并放回false
+    $timeout(function () {
+      $scope.errContent.msg = '';
+    }, 2000);
+    return false;
+  }
+  //end: msg
+
+
   widthFunctions();
 }
